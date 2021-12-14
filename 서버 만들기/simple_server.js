a6a7e0d9e07a127fb123858334b9065110002e2d
YunYoonBin <yubiny287@gmail.com>
2021-12-14 21:30:06 +0900

    node

--- 서버 만들기/simple_server.js
+++ 서버 만들기/simple_server.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-    res.write(' <h1>Node.js로 서버 만들기</h1>')
-    res.end('<p>http모듈 공부 중 .. </p>')
-})
-    .listen(8080, () => {
-        console.log("8080포트에서 서버 연결중 ..")
-    })
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log('8080포트에서 서버 실행중')
+});
