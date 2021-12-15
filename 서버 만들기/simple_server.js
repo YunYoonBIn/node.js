639a0f1c57dae441a6fa7bb36685fd9e9b7fbc56
YunYoonBin <yubiny287@gmail.com>
2021-12-15 20:40:55 +0900

    node

--- 서버 만들기/simple_server.js
+++ 서버 만들기/simple_server.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
+
+app.get('/home/write', (req, res) => {
+    res.sendFile(__dirname + "/write.html");
+});
+
+app.post('/newpost', (req, res) => {
+    res.send("전송완료")
+    console.log(req.body.title);
+    console.log(req.body.date);
+})
 
 app.listen(port, () => {
     console.log('8080포트에서 서버 실행중')
