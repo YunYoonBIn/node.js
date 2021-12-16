3a33b08ed65dd114e6115b32b0cd47b59899b7b2
YunYoonBin <yubiny287@gmail.com>
2021-12-17 03:09:52 +0900

    node

--- Todo/simple_server.js
+++ Todo/simple_server.js
@@ -8,26 +8,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var db;
 
 const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb+srv://yubiny289:dbqls0417@cluster0-qaxa3.mongodb.net/TodoApp?retryWrites=true&w=majority', (err, client) => {
+MongoClient.connect('mongodb+srv://yubiny289:dbqls0417@cluster0.dsbtf.mongodb.net/TodoApp?retryWrites=true&w=majority', (err, client) => {
     if (err) return console.log(err);
 
     db = client.db('TodoApp');
 
-    db.collection('post').insertOne('', (err, 결과) => {
-        console.log('저장완료')
-    });
-
-    app.listen(port, function () {
+    app.listen(port, () => {
         console.log('listening on 8080')
     });
 })
 
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/home/write', (req, res) => {
+app.get('/write', (req, res) => {
     res.sendFile(__dirname + "/write.html");
 });
 
@@ -35,4 +31,7 @@ app.post('/newpost', (req, res) => {
     res.send("전송완료")
     console.log(req.body.title);
     console.log(req.body.date);
+    db.collection('post').insertOne({ 제목: req.body.title, 날짜: req.body.date }, (err, 결과) => {
+        console.log('데이터 저장완료')
+    });
 })
