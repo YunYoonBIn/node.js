b7576ee2c2b349262aa1bb9b349e2afa7c170d9a
YunYoonBin <yubiny287@gmail.com>
2021-12-16 23:24:26 +0900

    node

--- 서버 만들기/simple_server.js
+++ 서버 만들기/simple_server.js
@@ -5,6 +5,24 @@ const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var db;
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://yubiny289:dbqls0417@cluster0-qaxa3.mongodb.net/TodoApp?retryWrites=true&w=majority', (err, client) => {
+    if (err) return console.log(err);
+
+    db = client.db('TodoApp');
+
+    db.collection('post').insertOne('', (err, 결과) => {
+        console.log('저장완료')
+    });
+
+    app.listen(port, function () {
+        console.log('listening on 8080')
+    });
+})
+
+
 app.get('/home', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
@@ -18,7 +36,3 @@ app.post('/newpost', (req, res) => {
     console.log(req.body.title);
     console.log(req.body.date);
 })
-
-app.listen(port, () => {
-    console.log('8080포트에서 서버 실행중')
-});
