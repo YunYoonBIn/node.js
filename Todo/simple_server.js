3944ca2b08969220a081883cae180c9de82ff07d
YunYoonBin <yubiny287@gmail.com>
2021-12-19 01:32:13 +0900

    node

--- Todo/simple_server.js
+++ Todo/simple_server.js
@@ -43,7 +43,28 @@ app.post('/newpost', (req, res) => {
     res.send("전송완료")
     console.log(req.body.title);
     console.log(req.body.date);
-    db.collection('post').insertOne({ 제목: req.body.title, 날짜: req.body.date }, (err, 결과) => {
-        console.log('데이터 저장완료')
+    db.collection('counter').findOne({ name: '게시물갯수' }, (err, client) => {
+        if (err) return console.log(err);
+        var 총게시물갯수 = client.totalpost
+
+        db.collection('post').insertOne({ _id: 총게시물갯수, 제목: req.body.title, 날짜: req.body.date }, (err, client) => {
+            if (err) return console.log(err);
+            console.log('데이터 저장완료');
+
+            db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalpost: 1 } }, (err, client) => {
+                if (err) return console.log(err);
+            })
+        });
     });
+});
+
+app.delete('/delete', (req, res) => {
+    console.log(req.body._id);
+    req.body._id = parseInt(req.body._id);
+    db.collection('post').deleteOne(req.body, (err, client) => {
+        console.log('삭제완료');
+        res.status(200).send({ message: "성공했습니다." });
+
+    })
 })
+
