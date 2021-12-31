40380ee5be052c10b0f50d5f79e4c678ac094578
YunYoonBin <yubiny287@gmail.com>
2021-12-31 22:09:50 +0900

    nodeJS

--- 회원가입 구현/server.js
+++ 회원가입 구현/server.js
@@ -9,9 +9,9 @@ const session = require('express-session');
 const crypto = require('crypto');
 const FileStore = require('session-file-store')(session);
 const cookieParser = require('cookie-parser');
-
+const res = require('express/lib/response');
 app.use(session({
-    secret: 'blackzat',
+    secret: 'yunyubin',
     resave: false,
     saveUninitialized: true,
     stoer: new FileStore,
@@ -30,9 +30,9 @@ const db = mysql.createConnection({
 
 db.connect();
 
-// 메인페이지
+// 메인 페이지
 
-// 아래와와 같이 만약 세선에 로그인이 되었다면 세션 값들을 보내서
+// 아래와 같이 만약 세선에 로그인이 되었다면 세션 값들을 보내서
 // 인식하게 처리를 함, 하지만 아니라면 그냥 화면을 출력하게 만듦.
 
 app.get('/', (req, res) => {
@@ -49,6 +49,46 @@ app.get('/', (req, res) => {
         });
     }
 });
+
+/* 회원가입 페이지
+
+아래와 같이 회원가입을 눌렀을 시 get으로 파일을 띄우고
+만약 회원가입창에서 회원가입을 하면
+post로 보내기에 post로 받아서
+회원가입창에 입력한 정보부터 다 가져온다.
+
+그리고 mysql에 접속을 한 뒤 그 데이터 값(길이로 판단)이 없으면
+회원가입을 시켜 데이터베이스에 저장한다.
+만약 있으면 실패로 하고 다시 돌아온다. */
+
+app.get('/register', (req, res) => {
+    console.log('회원가입 페이지')
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    console.log('회원가입 하는중')
+    const body = req.body;
+    const id = body.id;
+    const pw = body.pw;
+    const name = body.name;
+    const ago = body.ago;
+    const email = body.email;
+})
+
+db.query('select * from userdata where id=?', [id], (err, data) => {
+    if (data.length == 0) {
+        console.log('회원가입 성공')
+        db.query('insert into userdata(id, pw, name, ago, email) value(?,?,?,?,?)', [
+            id, pw, name, ago, email
+        ]);
+        res.redirect('/')
+    } else {
+        console.log('회원가입 실패')
+        res.send('<script> alert("회원가입 실패") </script>')
+        res.redirect('/login');
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
